cd50c0e67f99590570fe20b5f57fbf5b48cf0af5
TawqPlatform <151886835+TawqPlatform@users.noreply.github.com>
2024-01-17 05:09:10 +0300

    Create PrintEvens2.java

---
+++ PrintEvens2.java
@@ -0,0 +1,17 @@
+package PrintEvens1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tawq.pro
+ */
+public class PrintEvens1 {
+
+    public static void main(String[] args) {
+        System.out.println("Even numbers from 1 to 100: ");
+        for (int i = 2; i <= 100; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
