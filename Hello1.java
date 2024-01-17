dc718ceeac6ec71de35af80a1768833d5c83b40f
TawqPlatform <151886835+TawqPlatform@users.noreply.github.com>
2024-01-17 06:32:32 +0300

    Create Hello1.java

---
+++ Hello1.java
@@ -0,0 +1,14 @@
+package Hello1;
+
+
+/**
+ *
+ * @author Tawq.pro
+ */
+public class Hello1 {
+
+    public static void main(String[] args) {
+        String name = "Tawq";
+        System.out.println("Hello " + name);
+    }
+}
