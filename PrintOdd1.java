97e3ed11fd1916ba6d44ec93d0779fe155959034
TawqPlatform <151886835+TawqPlatform@users.noreply.github.com>
2024-01-18 06:06:40 +0300

    Update PrintOdd1.java

--- PrintOdd1.java
+++ PrintOdd1.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class PrintOdd1 {
 
     public static void main(String[] args) {
-        System.out.println("Even numbers from 1 to 100: ");
+        System.out.println("Odd numbers from 1 to 100: ");
         for (int i = 1; i <= 100; i+=2) {
             System.out.println(i);
         }
