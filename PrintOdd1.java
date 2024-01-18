af9b297d804ca6dfb98b91124152f09aaf60491b
TawqPlatform <151886835+TawqPlatform@users.noreply.github.com>
2024-01-18 06:03:49 +0300

    Create PrintOdd1.java

---
+++ PrintOdd1.java
@@ -0,0 +1,17 @@
+package PrintOdd1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tawq.pro
+ */
+public class PrintOdd1 {
+
+    public static void main(String[] args) {
+        System.out.println("Even numbers from 1 to 100: ");
+        for (int i = 1; i <= 100; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
