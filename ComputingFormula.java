e06e9e8ca019f6d731e179ddd929bdea0652a96d
TawqPlatform <151886835+TawqPlatform@users.noreply.github.com>
2024-01-18 05:19:38 +0300

    Create ComputingFormula.java

---
+++ ComputingFormula.java
@@ -0,0 +1,15 @@
+package ComputingFormula;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Tawq.pro
+ */
+public class ComputingFormula {
+    public static void main(String[] args) {
+        double formula = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+        System.out.println(formula);
+    }
+}
