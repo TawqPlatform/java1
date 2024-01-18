7590e2a7101404d99926fd6ca79b5afa99cac328
TawqPlatform <151886835+TawqPlatform@users.noreply.github.com>
2024-01-18 04:16:22 +0300

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+package Calculator;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Tawq.pro
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter the first number: ");
+        int num2 = in.nextInt();
+        
+        System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1-num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1*num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1/num2));
+        System.out.println(num1 + " % " + num2 + " = " + (num1%num2));
+        
+    }
+}
