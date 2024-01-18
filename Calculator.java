99ce626e3ab08941b2849cd7e03cedda9253ae61
TawqPlatform <151886835+TawqPlatform@users.noreply.github.com>
2024-01-18 04:19:27 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -19,7 +19,7 @@ public class Calculator {
         System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
         System.out.println(num1 + " - " + num2 + " = " + (num1-num2));
         System.out.println(num1 + " * " + num2 + " = " + (num1*num2));
-        System.out.println(num1 + " / " + num2 + " = " + (num1/num2));
+        System.out.println(num1 + " / " + num2 + " = " + (double)(num1/num2));
         System.out.println(num1 + " % " + num2 + " = " + (num1%num2));
         
     }
