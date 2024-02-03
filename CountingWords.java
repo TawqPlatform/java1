2331d974f48df005a854c9194a7480ccf615abf6
TawqPlatform <151886835+TawqPlatform@users.noreply.github.com>
2024-02-03 23:39:56 +0300

    Create CountingWords.java

---
+++ CountingWords.java
@@ -0,0 +1,38 @@
+package CountingWords;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tawq.pro
+ */
+public class CountingWords {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String inputString = in.nextLine();
+        int numOfWords = countWords(inputString);
+        System.out.println("\""+inputString + "\"" + " has " + numOfWords + " words");
+    }
+    
+    public static int countWords(String str) {
+        if (str.isEmpty()) {
+            return 0; // No vowels in an empty or null string
+        }
+
+        int wordsCount = 1; // for the first word
+
+        // to count the rest of the string 
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == ' ') 
+                wordsCount++;
+        }
+
+        return wordsCount;
+    }
+
+}
