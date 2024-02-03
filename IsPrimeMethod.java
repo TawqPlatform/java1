7652ec333727ca2ea5e380009f48b340f3998601
TawqPlatform <151886835+TawqPlatform@users.noreply.github.com>
2024-02-03 22:13:51 +0300

    Update IsPrimeMethod.java

--- IsPrimeMethod.java
+++ IsPrimeMethod.java
@@ -1,13 +1,16 @@
 package IsPrimeMethod;
-
+import java.util.Scanner;
 /**
  *
  * @author Tawq.pro
  */
 
 public class IsPrimeMethod {
+    
     public static void main(String[] args) {
-        int x = 5;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int x = in.nextInt();
         if(isPrime(x))
             System.out.println(x + " is a prime number");
         else 
